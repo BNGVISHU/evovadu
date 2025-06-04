4e30473976387dc973d3b4c4be899b819ccae94b
BNGVISHU <bngvishu@gmail.com>
2025-06-04 11:01:04 +0530

    asdjias

---
+++ client/dashboard/script.js
@@ -0,0 +1,26 @@
+async function main(){
+    try {
+        fetchAPISERVER = await fetch("https://evovadu-backend.onrender.com/profile",{
+            credentials : "include"
+        }).then(res => res.json()).then(data => {
+            refData = data['user'] // More refined 
+            if (data == null){
+                window.location.href = "../login/index.html"
+            }
+            else{
+                document.getElementById("username-button-dashbord").innerHTML = `<img src="${data['user'].profilePicture}" class="user-profile-image" id="main-user-profile" alt="user profile picture" >`;
+                document.getElementById("username-button-dashbord").innerHTML = `<img src="${data['user'].profilePicture}" class="user-profile-image" id="main-user-profile-tablet" alt="user profile picture" >`;
+            }
+            
+        })
+    } catch (error) {
+        console.log(`Fetch Failed ${error}`)
+        window.location.href = "../login/index.html"
+        
+    }
+
+
+}
+
+
+main()
